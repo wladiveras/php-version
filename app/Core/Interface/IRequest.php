f67312f8cc3561468e7dfabb4cc8c11560a362b9
Wladi Veras <wladinart@gmail.com>
2024-02-09 21:55:02 -0300

    refactor: :zap: add get functions interfaces

--- app/Core/Interface/IRequest.php
+++ app/Core/Interface/IRequest.php
@@ -6,4 +6,7 @@ namespace Core\Interface;
 interface IRequest
 {
     public function getBody();
+    public function getQuery();
+    public function getString($name);
+    public function getJson($name);
 }
